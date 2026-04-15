af7136f3235e663bc3a7d4b6c92e111d4ec7b617
pabloX <lonwabo.mnyaiza@gmail.com>
2026-04-15 21:21:31 +0200

    :package: add auth middleware.

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,16 @@
+require("dotenv");
+const jwt = require("jsonwebtoken");
+
+export default function authenticate(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied. No token to redeem.");
+
+  try {
+    const privateKey = process.env.TOKEN;
+    const decodedToken = jwt.verify(token, privateKey);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+}
