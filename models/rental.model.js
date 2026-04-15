b08596fb21e7cfd348e456a60d6e7ba51f7d5dca
pabloX <lonwabo.mnyaiza@gmail.com>
2026-04-15 18:27:16 +0200

    :safety_vest: make validation more robust.

--- models/rental.model.js
+++ models/rental.model.js
@@ -1,39 +1,40 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
-const { customerSchema } = require('./customer.model');
-const { movieDto } = require('./movie.model');
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const { customerSchema } = require("./customer.model");
+const { movieDto } = require("./movie.model");
+Joi.objectId = require("joi-objectid");
 
-const name = 'Rental';
+const name = "Rental";
 const schema = new mongoose.Schema({
-    customer: {
-        type: customerSchema,
-        required: true,
-    },
-    movie: {
-        type: movieDto,
-        required: true,
-    },
-    checkoutDate: {
-        type: Date,
-        default: Date.now,
-    },
-    returnedDate: {
-        type: Date,
-    },
-    rentalFee: {
-        type: Number,
-        min: 1,
-        required: true,
-    }
+  customer: {
+    type: customerSchema,
+    required: true,
+  },
+  movie: {
+    type: movieDto,
+    required: true,
+  },
+  checkoutDate: {
+    type: Date,
+    default: Date.now,
+  },
+  returnedDate: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
 });
 const model = mongoose.model(name, schema);
 
 function validateUsingJoi(input) {
-    const schema = Joi.object({
-        customer: Joi.string().required(),
-        movie: Joi.string().required(),
-    })
-} 
+  const schema = Joi.object({
+    customer: Joi.objectId().required(),
+    movie: Joi.objectId().required(),
+  });
+}
 
 module.exports.Rental = model;
 module.exports.validate = validateUsingJoi;
