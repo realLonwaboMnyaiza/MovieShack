b6ae0efefef86e6db3215261036da54fed2fe38d
pabloX <lonwabo.mnyaiza@gmail.com>
2026-04-16 11:20:13 +0200

    :bug::package: fix issue, use common js to export module.

--- middleware/authentication.middleware.js
+++ middleware/authentication.middleware.js
@@ -1,16 +1,16 @@
-require("dotenv");
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-export default function authenticate(req, res, next) {
+module.exports = function authenticate(req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token to redeem.");
 
   try {
-    const privateKey = process.env.TOKEN;
+    const privateKey = process.env.KEY;
     const decodedToken = jwt.verify(token, privateKey);
     req.user = decodedToken;
     next();
   } catch (error) {
     res.status(400).send("Invalid token");
   }
-}
+};
