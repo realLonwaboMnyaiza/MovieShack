f5ef64374b7a60c3be13b7c28cedfcc9e32f2a3b
pabloX <lonwabo.mnyaiza@gmail.com>
2026-04-18 14:28:54 +0200

    :white_check_mark: adding unit test.

---
+++ unit_tests/user.model.test.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/user.model");
+
+describe("when user.generateAuthenticationToken is called", () => {
+  it("should return a valid JWT.", () => {
+    // arrange.
+    const privateKey = process.env.KEY;
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+
+    // act.
+    const user = new User(payload);
+    const token = user.generateAuthenticationToken();
+    const decodedToken = jwt.verify(token, privateKey);
+
+    // assert.
+    expect(decodedToken).toMatchObject(payload);
+  });
+});
