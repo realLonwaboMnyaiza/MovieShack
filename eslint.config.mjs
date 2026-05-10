4c564bf80918b5ce52e7e532b93a1dd44f1460d6
pabloX <lonwabo.mnyaiza@gmail.com>
2026-05-10 17:04:50 +0200

    :wrench: config jest as plugin and allow keywords.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { js, jest } from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js, jest },
+    extends: ['js/recommended'],
+    languageOptions: { globals: [globals.node, globals.jest, globals.es2021] },
+    rules: {
+      'no-unused-vars': 'warn',
+      'prefer-const': 'warn',
+      'space-before-function-paren': 'off',
+      indent: ['error', 2],
+      semi: ['error', 'always'],
+    },
+  },
 ]);
