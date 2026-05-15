442f03625428c93f0c69d13adf1346d55454ff13
pabloX <lonwabo.mnyaiza@gmail.com>
2026-05-15 23:03:43 +0200

    :fire::wrench: remove jest configurations.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import { js, jest } from '@eslint/js';
+import { js } from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js, jest },
+    plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: [globals.node, globals.jest, globals.es2021] },
+    languageOptions: { globals: [globals.node, globals.es2021] },
     rules: {
       'no-unused-vars': 'warn',
       'prefer-const': 'warn',
